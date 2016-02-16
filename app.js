a1429ac63c4bf21c7666657ec1d25e9250deef6f
mjimaz <m.aljimaz@gmail.com>
2016-02-15 17:01:55 -0800

    rearrange folder content add two tabs, search and current tv shows hide/show based on tab selection

--- app.js
+++ app.js
@@ -10,29 +10,63 @@ app.factory('IMDBapi', function($http) {
 	http://api.themoviedb.org/3/search/tv?api_key=API_KEY&query=game
 	
 	*/
-  var searchTVShow = function( tvshow ) {
+  var searchTVShows = function( tvshow ) {
     return $http({
       method: 'GET',
-      url: 'http://api.themoviedb.org/3/search/tv?api_key=8353f37b04124bdd138f12db75c775dd&query='+tvshow,
+      url: 'http://api.themoviedb.org/3/search/tv?api_key=8353f37b04124bdd138f12db75c775dd&query='+tvshow
     })
-    .then(function (resp) {
+    .then(function ( resp ) {
       return resp.data;
     });
   };
 
+  var getTVShowInfo = function( tvshowid ) {
+  	return $http({
+      method: 'GET',
+      url: 'http://api.themoviedb.org/3/tv/'+tvshowid+'?api_key=8353f37b04124bdd138f12db75c775dd'
+    })
+    .then(function ( resp ) {
+      return resp.data;
+    });
+  }
+    //http://api.themoviedb.org/3/tv/id
+
   return {
-    searchTVShow: searchTVShow,
+    searchTVShows: searchTVShows,
+    getTVShowInfo: getTVShowInfo
   }; 
 
 });  // end of factory IMDBapi
 
 app.controller('TVShowsController', function($scope, IMDBapi) {
 
-	$scope.tvshows;
-
-  $scope.getTVShow = function( tvshow ) {
-    IMDBapi.searchTVShow( 'the walking dead' )
+  $scope.getTVShowsListByName = function( tvshow ) {
+    IMDBapi.searchTVShows( 'the walking dead' )
     .then(function (tvshows){
+    	console.log(tvshows.results[0].id);
+
+    	$scope.getTVShowDetails(tvshows.results[0].id);
     });
-  }
+  };
+
+  $scope.getTVShowDetails = function( tvshowid ) {
+    IMDBapi.getTVShowInfo( tvshowid )
+    .then(function (tvshowdetails){
+    	console.log('tvshowdetails:', tvshowdetails);
+    });
+  };
+
 });  // end of controller TVShowsController
+
+app.controller('TabController', function( $scope ) {
+	$scope.tab = 0;
+
+	$scope.setTab = function ( tab ) {
+		$scope.tab = tab;
+	};
+
+	$scope.isSet = function ( tab ) {
+		return $scope.tab === tab;
+	};
+
+}); // end of controller TabController
